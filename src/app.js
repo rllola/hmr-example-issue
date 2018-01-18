ebe485e7efbea8c794f7594951c7b277fc56a05a
rllola <rllola80@gmail.com>
2018-01-18 17:45:27 +0100

    Test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Home from './home'
 import Store from './store'
+import { inject } from 'mobx-react'
 
-const App = (props) => {
-  console.log(props.store instanceof Store)
-  console.log(store)
-  return (<Home store={props.store} />)
-}
+const App = inject('store')((props) => {
+  return (<Home city={props.store.city} />)
+})
 
 export default App
--- src/home.js
+++ src/home.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
-import { observer } from 'mobx-react'
-import Store from './store'
+import PropTypes from 'prop-types'
 import { hot } from 'react-hot-loader'
 
-@observer
 class Home extends Component {
   render () {
-    console.log(this.props.store instanceof Store)
-
     return (
       <div>
-        <h2>Welcome to {this.props.store.city}, Lola !</h2>
+        <h2>Welcome to {this.props.city}, Bedeho !</h2>
       </div>)
   }
+}
+
+Home.propTypes = {
+  city: PropTypes.string.isRequired
 }
 
 export default Home
